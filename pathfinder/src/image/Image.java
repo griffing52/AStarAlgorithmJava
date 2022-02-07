0db85fd9964c0b30a8b53a06ca364bfe002774f3
griffing52 <emailgriffinnow@gmail.com>
2022-02-06 20:48:49 -0800

    Test

--- pathfinder/src/image/Image.java
+++ pathfinder/src/image/Image.java
@@ -24,6 +24,7 @@ public class Image {
         }
 
         File file = new File("path.png");
+        // ImageIO.write(buffImg, "png", file);
         ImageIO.write(buffImg, "png", file);
         // Arrays.asList(arr)
         //     .stream()
